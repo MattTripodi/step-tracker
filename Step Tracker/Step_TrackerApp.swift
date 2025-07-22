c216cf59b810b22061a1cad2aa712a76ed1046e8
Matthew Tripodi <mtripodi1027@gmail.com>
2025-07-21 22:20:36 -0400

    Created Xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Matthew Tripodi on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
